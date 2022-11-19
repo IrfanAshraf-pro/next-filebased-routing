e55993996ae794f08b71b75d8784f97bdb01ecd4
IrfanAshraf-pro <irfanashraf4090@gmail.com>
2022-11-19 20:46:00 +0500

    navigating programatically with router.push

--- pages/blog/[authorname]/index.js
+++ pages/blog/[authorname]/index.js
@@ -2,7 +2,19 @@ import { useRouter } from "next/router";
 const index = () => {
 	const router = useRouter();
 	console.log(router.query);
-	return <div>Author name is : {router.query.authorname}</div>;
+	const pressHandler = () => {
+		// router.push("/blog/irfan/A")
+		router.push({
+			pathname: "/blog/[authorname]/[postid]",
+			query: { authorname: "irfan", postid: "A" },
+		});
+	};
+	return (
+		<div>
+			<h1>Author name is : {router.query.authorname}</h1>
+			<button onClick={pressHandler}>Load project A</button>
+		</div>
+	);
 };
 
 export default index;
